fe88223f751bb03af4aa27f89feaa473b7b99731
Vinícius Gonçalves <103079267+gonssalves@users.noreply.github.com>
2022-09-07 01:52:10 -0300

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,71 +1,56 @@
-class PilhaVazia(Exception):
-    pass
+#Miniteste feito por: Vinícius Gonçalves de Oliveira e Vitor Melo da Silva
 
 class SenhaInvalida(Exception):
-    pass
+  pass
 
 class PilhaArray:
 
-    def __init__(self):
-        self._items = []
-        self.senha = []
-        self.maiusculo = []
-        self.minusculo = []
-        
-    def __len__(self):
-        return len(self._items)
+  def __init__(self):
+    self.senha = []
+    self.maiusculo = []
+    self.minusculo = []
+      
+  def __len__(self):
+    return len(self.senha)
 
-    def size(self):
-        return self.__len__()
+  def push(self, e):
+    self.senha.append(e)
 
-    def is_empty(self):
-        return len(self._items) == 0
-
-    def push(self, e):
-        self._items.append(e)
-
-    def top(self):
-        if self.is_empty():
-            raise PilhaVazia('pilha vazia')
-        return self._items[-1]
-
-    def pop(self):
-        if self.is_empty():
-            raise PilhaVazia('pilha vazia')
-        return self._items.pop()
-
-    def valida_senha(self, i=0):
-        if len(self._items) == i:    
-            if len (self.maiusculo) == 0 or len(self.minusculo) == 0 or len(self.maiusculo) != len(self.minusculo):
-                raise SenhaInvalida('senha inválida')
-            else:
-                self.count = 0
-                for j,k in zip(self.maiusculo, self.minusculo):
-                    if j.lower() == k:
-                        self.count +=1  
-            return 'Senha válida'
-        elif self._items[i] == self._items[i].upper():
-            self.maiusculo.append(self._items[i])
-            return self.valida_senha(i+1)
-        elif self._items[i] == self._items[i].lower():
-            self.minusculo.append(self._items[i])
-            return self.valida_senha(i+1)
-        
+  def valida_senha(self, i = 0):
+    if len(self.senha) == i:    
+      if len(self.maiusculo) != len(self.minusculo) or len(self.senha) == 0:
+        raise SenhaInvalida('senha inválida')
+      else: 
+        return 'Senha válida'
+    elif self.senha[i] == self.senha[i].upper():
+      self.maiusculo.append(self.senha[i])
+      return self.valida_senha(i+1)
+    elif self.senha[i] == self.senha[i].lower():
+      if self.senha[i].upper() == self.maiusculo[-1]:
+        self.minusculo.append(self.senha[i])
+        self.maiusculo.pop(-1)
+        self.minusculo.pop(-1)
+        return self.valida_senha(i+1)
+      else:
+        raise SenhaInvalida('senha INVALIDA')        
               
 if __name__ == '__main__':
-    try:
-        s = PilhaArray()
-        s.push('A')
-        s.push('B')
-        s.push('C')
-        s.push('c')
-        s.push('D')
-        s.push('E')
-        s.push('e')
-        s.push('d')
-        s.push('b')
-        s.push('a')
+  try:
+    s = PilhaArray()
+    s.push('A')
+    s.push('B')
+    s.push('C')
+    s.push('c')
+    s.push('D')
+    s.push('E')
+    s.push('e')
+    s.push('d')
+    s.push('b')
+    s.push('a')
+    print(s.valida_senha())
+  except:
+    print('Senha inválida')
     
-        print(s.valida_senha())
-    except:
-        print('Senha inválida')
+    
+# ['A', 'B', 'C', 'D', 'E']
+# ['c', 'e', 'd', 'b', 'a']
